e94d2b5897ee2e82ceabf4b1cca28a3c49d8112c
kiu-art <kiusinh098@gmail.com>
2026-07-12 11:02:20 +0530

    fix: update package.json to include dns dependency and correct import path in server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ setServer(["8.8.8.8","1.1.1.1"]);
 import { configDotenv } from "dotenv";
 configDotenv();
 
-import { app } from "./src/app";
+import { app } from "./src/app.js";
 
 app.listen(process.env.PORT || 5000, ()=>{
   console.log(`Server started at port ${process.env.PORT || 5000}`)
